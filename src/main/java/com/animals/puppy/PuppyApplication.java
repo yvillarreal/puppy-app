0d8eeb1a7da599f05fd7d4fd06dd287e3497d7a0
yvillarreal <yamil.villarreal@outlook.com>
2021-11-30 15:14:52 -0600

    first commit

--- src/main/java/com/animals/puppy/PuppyApplication.java
+++ src/main/java/com/animals/puppy/PuppyApplication.java
@@ -2,9 +2,18 @@ package com.animals.puppy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class PuppyApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PuppyApplication.class, args);
