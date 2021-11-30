50ca9c99d39016d511f2b542278f74b69fa7bf5f
yvillarreal <yamil.villarreal@outlook.com>
2021-11-30 14:54:15 -0600

    first commit

---
+++ src/main/java/com/animals/puppy/PuppyApplication.java
@@ -0,0 +1,13 @@
+package com.animals.puppy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animals/puppy/PuppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animals.puppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
