3293876ea684b3cdcb0a250bd6eae71812932b69
yvillarreal <yamil.villarreal@outlook.com>
2021-11-30 15:21:20 -0600

    first commit

---
+++ src/main/java/com/animals/puppy/IndexController.java
@@ -0,0 +1,12 @@
+package com.animals.puppy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "Hola mundo.";
+    }
+}
--- src/main/java/com/animals/puppy/PuppyApplication.java
+++ src/main/java/com/animals/puppy/PuppyApplication.java
@@ -2,14 +2,12 @@ package com.animals.puppy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class PuppyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PuppyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PuppyApplication.class, args);
+    }
 
 }
