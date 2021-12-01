e68a311a9d2f48ceafeec540a874d0db80fd9253
yvillarreal <yamil.villarreal@outlook.com>
2021-12-01 11:49:58 -0600

    first commit

--- src/main/java/com/animals/puppy/controller/ClienteController.java
+++ src/main/java/com/animals/puppy/controller/ClienteController.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class ClienteController {
 
     @Autowired
